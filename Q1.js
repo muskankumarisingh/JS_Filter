3a67206310ba41cdabbf473bc685fba9bbde5aa4
muskankumarisingh <81345123+muskankumarisingh@users.noreply.github.com>
2021-10-16 10:25:10 +0530

    Add files via upload

---
+++ Q1.js
@@ -0,0 +1,5 @@
+const array1=[5,12,8,130,44];
+const newArr=array1.filter((elem)=>{
+    return elem>150;
+})
+console.log(newArr);
---
+++ Q3.js
@@ -0,0 +1,6 @@
+var numbers = [1, 2, 6, 8, 12, 14];
+var a= numbers.filter(element => element>5)
+console.log(a);
+
+//output:[6,8,12,14]
+ 
